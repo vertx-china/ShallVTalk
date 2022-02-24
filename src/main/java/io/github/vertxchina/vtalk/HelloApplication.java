7143fcf5f066ed821739996a66664c202db23920
chengenzhao <chengen.zhao@mail.mcgill.ca>
2022-02-24 11:19:46 +0800

    for testing

--- src/main/java/io/github/vertxchina/vtalk/HelloApplication.java
+++ src/main/java/io/github/vertxchina/vtalk/HelloApplication.java
@@ -1,7 +1,9 @@
 package io.github.vertxchina.vtalk;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -11,11 +13,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException {
-    Text text = new Text("hello world");
+    Label text = new Label("hello world");
     Scene scene = new Scene(new Pane(text), 320, 240);
     stage.setTitle("Hello!");
     stage.setScene(scene);
     stage.show();
+
+    var animationTimer = new AnimationTimer(){
+      @Override
+      public void handle(long now) {
+        text.setLayoutX(text.getLayoutX()+1);
+        text.setLayoutY(text.getLayoutY()+1);
+      }
+    };
+    animationTimer.start();
+
   }
 
   public static void main(String[] args) {
