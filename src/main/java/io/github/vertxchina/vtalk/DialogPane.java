3348ce764884b52cdceb8bed9abc7cccdb1389e6
chengenzhao <chengen.zhao@mail.mcgill.ca>
2022-02-28 17:14:17 +0800

    添加concurrency段落到yml 防止多次提交时候造成冲突 后提交的会自动取消前提交还未完成的build

--- src/main/java/io/github/vertxchina/vtalk/DialogPane.java
+++ src/main/java/io/github/vertxchina/vtalk/DialogPane.java
@@ -2,6 +2,7 @@ package io.github.vertxchina.vtalk;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeType;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
