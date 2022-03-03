5f7ec7da5ff2b50fe1385028cd94cc685ab43ab7
chengenzhao <chengen.zhao@mail.mcgill.ca>
2022-03-03 17:43:27 +0800

    一个非常初级的可以读取图片的版本

--- src/main/java/io/github/vertxchina/vtalk/dialogpane/CenterPane.java
+++ src/main/java/io/github/vertxchina/vtalk/dialogpane/CenterPane.java
@@ -5,6 +5,7 @@ import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -12,6 +13,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
+import java.net.URL;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -29,16 +31,23 @@ public class CenterPane extends ScrollPane {
   public void appendChatHistory(JsonNode node) {
     var nickname = node.path("nickname");
     var time = node.path("time").asText(ZonedDateTime.now().format(timeFormatter));
-    var color = node.path("color").asText("#000");
-    var label = new Label();
+    var msgHead = new Label();
+    var msgBody = new Label();
     var nn = nickname.isMissingNode() ? "我" : nickname.asText();
-    label.setText(nn + " " + time + System.lineSeparator() + node.path("message").asText(" "));
-    label.setTextFill(Color.web(color));
-    label.setPadding(new Insets(5));
-    label.setLineSpacing(3);
+    msgHead.setText(nn + " " + time);
+    var message = node.path("message").asText(" ").trim();
+    if(message.startsWith("http")&&(message.endsWith("png")||message.endsWith("jpg")||message.endsWith("gif"))){
+      msgBody.setGraphic(new ImageView(message));
+    }else{
+      msgBody.setText(message);
+      var color = node.path("color").asText("#000");
+      msgBody.setTextFill(Color.web(color));
+    }
+    msgBody.setPadding(new Insets(5));
+    msgBody.setLineSpacing(3);
     if (nickname.isMissingNode())
-      label.setBackground(new Background(new BackgroundFill(Color.web("#b3e6b3"), new CornerRadii(5), Insets.EMPTY)));
+      msgBody.setBackground(new Background(new BackgroundFill(Color.web("#b3e6b3"), new CornerRadii(5), Insets.EMPTY)));
     Platform.runLater(() ->
-        chatHistory.getChildren().addAll(label, new Text(System.lineSeparator() + System.lineSeparator())));//nameText, new Text(System.lineSeparator()), msgLabel
+        chatHistory.getChildren().addAll(msgHead, new Text(System.lineSeparator()), msgBody, new Text(System.lineSeparator() + System.lineSeparator())));//nameText, new Text(System.lineSeparator()), msgLabel
   }
 }
